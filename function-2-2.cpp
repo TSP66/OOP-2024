ca1f81bf5e3d4d6946a6e214e1cc6c439a85157c
TSP66 <62830753+TSP66@users.noreply.github.com>
2024-08-01 13:25:49 +0930

    Bug fixes

--- function-2-2.cpp
+++ function-2-2.cpp
@@ -3,7 +3,7 @@
 
 int binary_to_int(int binary_digits[], int number_of_digits){
     int sum = 0;
-    for (int i = number_of_digits-1; i >= 0; i++){
+    for (int i = number_of_digits-1; i >= 0; i--){
         sum += binary_digits[0] * pow(2, i);
     }
     return sum;
