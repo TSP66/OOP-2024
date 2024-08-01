f26dab4dfd054b24d5860c3c90db8a9894fc7ac2
TSP66 <62830753+TSP66@users.noreply.github.com>
2024-08-01 13:23:59 +0930

    Bug fix 1-5

--- function-1-5.cpp
+++ function-1-5.cpp
@@ -2,6 +2,6 @@
 
 void print_summed(int array1[3][3],int array2[3][3]){
     for (int x = 0; x < 3; x++){
-        std::cout << array1[x][0] + array2[x][0] << " " << array1[x][1] + array2[x][0]<< " " << array1[x][2] + array2[x][0]<< std::endl;
+        std::cout << array1[x][0] + array2[x][0] << " " << array1[x][1] + array2[x][1]<< " " << array1[x][2] + array2[x][2]<< std::endl;
     }
 }
