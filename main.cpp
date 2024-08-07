f8368581ece19b39fb6964b4dd75826085ec0c03
TSP66 <62830753+TSP66@users.noreply.github.com>
2024-08-07 09:51:53 +0930

    Will that stop it complaining?

--- main.cpp
+++ main.cpp
@@ -4,38 +4,6 @@
 
 #include "workshop.h"
 
-
-void changeValue(double * v){
-    *v = 42.0;
-}
-
-void printArray(double*array, int length){
-    std::cout << "[";
-    for (int i = 0; i < length; i++){
-        std::cout << array[i] ;
-        if (i == length-1){
-            std::cout << ", ";
-        } else{
-            std::cout << "]\n";
-        }
-    }
-}
-
-double arrayMax(double*array, int length){
-    if (length <= 1) return array[0];
-    double maximum = std::max(array[0],array[1]);
-    for (int i = 2; i < length; i++){
-        maximum = std::max(maximum, array[i]);
-    }
-    return maximum;
-}
-
-double * dynamicArray(int length, double intial){
-    double * array = new double[length];
-    for (int i = 0; i < length; i++) array[i] = intial;
-    return array;
-}
-
 int main(){
     //Question 2
     double value = 0.0;
--- workshop.h
+++ workshop.h
@@ -1,9 +1,35 @@
 //workshop.h
+#include <iostream>
+#include <cassert>
+#include <algorithm>
 
-void changeValue(double * v);
+void changeValue(double * v){
+    *v = 42.0;
+}
 
-void printArray(double*array, int length);
+void printArray(double*array, int length){
+    std::cout << "[";
+    for (int i = 0; i < length; i++){
+        std::cout << array[i] ;
+        if (i == length-1){
+            std::cout << ", ";
+        } else{
+            std::cout << "]\n";
+        }
+    }
+}
 
-double arrayMax(double*array, int length);
+double arrayMax(double*array, int length){
+    if (length <= 1) return array[0];
+    double maximum = std::max(array[0],array[1]);
+    for (int i = 2; i < length; i++){
+        maximum = std::max(maximum, array[i]);
+    }
+    return maximum;
+}
 
-double * dynamicArray(int length, double intial);
+double * dynamicArray(int length, double intial){
+    double * array = new double[length];
+    for (int i = 0; i < length; i++) array[i] = intial;
+    return array;
+}
